d1eb1dd2c8617adbf17824516717e020cf0c6334
Gabriel Jaeger <23436587+gabrieltjaeger@users.noreply.github.com>
2022-08-25 14:19:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,24 @@
-# Simulador de Substituição de Páginas na Memória
-from collections import OrderedDict
-from operator import index
-from re import M, T
+from collections import OrderedDict, Counter
 
 
-class Memoria:
-    def __init__(self, numero_molduras):
-        self.numero_molduras = numero_molduras
-        self.molduras = []
-    
-    def cheia(self):
-        return len(self.molduras) == self.numero_molduras
-
-    def __str__(self):
-        return str(self.molduras)
-
-    def __repr__(self):
-        return str(self.molduras)
-
-    def __contains__(self, pagina):
-        return pagina in self.molduras
-
-    def __getitem__(self, index):
-        return self.molduras[index]
-
-    def __setitem__(self, index, pagina):
-        self.molduras[index] = pagina
-
-    def append(self, pagina):
-        self.molduras.append(pagina)
-
-    def pop(self, index=-1):
-        return self.molduras.pop(index)
-
-    def index(self, pagina):
-        return self.molduras.index(pagina)
-        
-        
-
-def fifo(numero_molduras, numero_paginas, sequencia_de_acesso):
-    molduras = Memoria(numero_molduras)
-    cont = 0
+def fifo(numero_molduras: int, sequencia_de_acesso: list) -> int:
+    molduras = []
+    trocas_de_paginas = 0
     while len(sequencia_de_acesso) > 0:
         pagina = sequencia_de_acesso.pop(0)
         if pagina in molduras:
             continue
         if pagina not in molduras:
-            if molduras.cheia():
+            if len(molduras) == numero_molduras:
                 molduras.pop(0)
             molduras.append(pagina)
-            cont += 1
-    return cont
+            trocas_de_paginas += 1
+    return trocas_de_paginas
 
-def mru(numero_molduras, numero_paginas, sequencia_de_acesso):
+
+def mru(numero_molduras: int, sequencia_de_acesso: list) -> int:
     molduras = {}
-    cont = 0
+    trocas_de_paginas = 0
     tempo = -1
     while len(sequencia_de_acesso) > 0:
         tempo += 1
@@ -65,14 +29,15 @@ def mru(numero_molduras, numero_paginas, sequencia_de_acesso):
             else:
                 molduras.pop(min(molduras, key=molduras.get))
                 molduras[pagina] = tempo
-            cont += 1
+            trocas_de_paginas += 1
         else:
             molduras[pagina] = tempo
-    return cont
+    return trocas_de_paginas
 
-def nuf(numero_molduras, numero_paginas, sequencia_de_acesso):
-    molduras = {}
-    cont = 0
+
+def nuf(numero_molduras: int, sequencia_de_acesso: list) -> int:
+    molduras = OrderedDict()
+    trocas_de_paginas = 0
     while len(sequencia_de_acesso) > 0:
         pagina = sequencia_de_acesso.pop(0)
         if pagina not in molduras.keys():
@@ -83,43 +48,60 @@ def nuf(numero_molduras, numero_paginas, sequencia_de_acesso):
                 minimo = min(molduras, key=molduras.get)
                 molduras.pop(minimo)
                 molduras[pagina] = 0
-            cont += 1
+            trocas_de_paginas += 1
         else:
             molduras[pagina] += 1
-    return cont
+    return trocas_de_paginas
 
-def otimo(numero_molduras, numero_paginas, sequencia_de_acesso):
-    return 0
 
-def menor_resultado(resultados):
-    return min(resultados, key=resultados.get)
-   
+def otimo(numero_molduras: int, sequencia_de_acesso: list) -> int:
+    molduras = OrderedDict()
+    trocas_de_paginas = 0
+    for i, pagina in enumerate(sequencia_de_acesso):
+        molduras = OrderedDict(
+            {k: v-1 for k, v in molduras.items() if (v > 1)})
+        if pagina in molduras:
+            if pagina in sequencia_de_acesso[i+1:]:
+                molduras[pagina] = sequencia_de_acesso.index(pagina, i+1) - i+1
+            continue
+        trocas_de_paginas += 1
+        if len(molduras) >= numero_molduras:
+            molduras = OrderedDict(
+                sorted(molduras.items(), key=lambda x: x[1]))
+            molduras.popitem()
+        if pagina in sequencia_de_acesso[i+1:]:
+            molduras[pagina] = sequencia_de_acesso.index(pagina, i+1) - i+1
+    return trocas_de_paginas
+
 
 def main():
-    # nome_arquivo_entrada = input("Digite o nome do arquivo de entrada: ")
-    # with open(f'arquivos/entrada/{nome_arquivo_entrada}', 'r') as arquivo:
+    saida_log = ''
     with open('arquivos/entrada/inMemoria', 'r') as arquivo:
         for linha in arquivo:
             linha = linha.strip()
             linha = linha.split('|')
             numero_molduras = int(linha[0])
-            numero_paginas = int(linha[1])
-            sequencia_de_acesso = linha[2].split(' ')
-            sequencia_de_acesso = [int(i) for i in sequencia_de_acesso]
-            
-            resultados = {
-                'fifo': fifo(numero_molduras, numero_paginas, list(sequencia_de_acesso)),
-                'mru': mru(numero_molduras, numero_paginas, list(sequencia_de_acesso)),
-                'nuf': nuf(numero_molduras, numero_paginas, list(sequencia_de_acesso)),
-            }
+            sequencia_de_acesso = list(map(int, linha[2].split(' ')))
 
-            resultado_otimo = otimo(numero_molduras, numero_paginas, sequencia_de_acesso)
+            resultados = {
+                'FIFO': fifo(numero_molduras, list(sequencia_de_acesso)),
+                'MRU': mru(numero_molduras, list(sequencia_de_acesso)),
+                'NUF': nuf(numero_molduras, list(sequencia_de_acesso)),
+                'OTIMO': otimo(numero_molduras, list(sequencia_de_acesso))
+            }
+            resultado_otimo = resultados.pop('OTIMO')
             melhor_resultado = min(resultados, key=resultados.get)
             empate = False
-            if resultados[melhor_resultado] == resultado_otimo:
+            count = Counter(resultados.values())
+            melhor_resultado_repete = count[resultados[melhor_resultado]] > 1
+            melhor_resultado_igual_ao_otimo = resultados[melhor_resultado] == resultado_otimo
+            if melhor_resultado_repete or melhor_resultado_igual_ao_otimo:
                 empate = True
-            print(f'{resultados["fifo"]}|{resultados["mru"]}|{resultados["nuf"]}|{resultado_otimo}|{"empate" if empate else melhor_resultado}')
-                
+            resultados = f'{resultados["FIFO"]}|{resultados["MRU"]}|{resultados["NUF"]}|{resultado_otimo}|{"empate" if empate else melhor_resultado}'
+            saida_log += f'{resultados}\n'
+            print(resultados)
+    with open('arquivos/saida/outMemoria', 'w') as saida:
+        saida.write(saida_log)
 
 
 if __name__ == '__main__':
